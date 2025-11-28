99c3b7296791978a2eab964a0cdda8ce43481d8d
viniciusCecat <vcecatto@minha.fag.edu.br>
2025-11-27 23:02:16 -0300

    Commit 13
    getCharge movido para subclasses; Price agora é abstrata.

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -1,6 +1,12 @@
 public class NewReleasePrice extends Price {
+
     @Override
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
     }
 }
