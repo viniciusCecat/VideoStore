5ef54e56bde3321107d088c25ce1dd116eb62bb7
viniciusCecat <vcecatto@minha.fag.edu.br>
2025-11-27 22:10:13 -0300

    Commit 3
    Rename: parâmetro do método amountFor renomeado para aRental

--- src/Customer.java
+++ src/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
 
         for (Rental each : _rentals) {
 
-            double thisAmount = amountFor(each); // ← AGORA CHAMA O NOVO MÉTODO
+            double thisAmount = amountFor(each); // continua igual
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -49,25 +49,25 @@ public class Customer {
         return result.toString();
     }
 
-    // MÉTODO EXTRAÍDO (COMMIT 2)
-    private double amountFor(Rental each) {
+    // MÉTODO AJUSTADO (Commit 3)
+    private double amountFor(Rental aRental) {
         double thisAmount = 0;
 
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (aRental.getDaysRented() > 2)
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
                 break;
 
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += aRental.getDaysRented() * 3;
                 break;
 
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (aRental.getDaysRented() > 3)
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
 
