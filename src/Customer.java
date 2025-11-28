0512375801c6acdd095d813faa608763ade60428
viniciusCecat <vcecatto@minha.fag.edu.br>
2025-11-27 22:16:27 -0300

    Commit 4
    Move and Rename Method: amountFor movido para Rental e renomeado para getCharge

--- src/Customer.java
+++ src/Customer.java
@@ -25,52 +25,23 @@ public class Customer {
 
         for (Rental each : _rentals) {
 
-            double thisAmount = amountFor(each); // continua igual
+            double thisAmount = each.getCharge();
 
-            // add frequent renter points
             frequentRenterPoints++;
 
-            // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
                 frequentRenterPoints++;
             }
 
-            // show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t")
                     .append(thisAmount).append("\n");
 
             totalAmount += thisAmount;
         }
 
-        // add footer lines
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
 
         return result.toString();
-    }
-
-    // MÉTODO AJUSTADO (Commit 3)
-    private double amountFor(Rental aRental) {
-        double thisAmount = 0;
-
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2)
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return thisAmount;
     }
 }
